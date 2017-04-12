6536de2a54262e2fdf12d4b75486682d31b31d5d
Gi-yong Kwon <handgear05@gmail.com>
2017-04-12 20:41:08 +0900

    Change to dummy head version

--- search.cpp
+++ search.cpp
@@ -13,8 +13,10 @@
 
 Search::Search()
 {
-    // Initialize an empty List
-    head = 0;
+    // Initialize an empty list
+    head = new Node;
+    assert(head);
+    head->next = 0;
     tail = 0;
     current = 0;
 }
@@ -33,45 +35,38 @@ void Search::insertHistory(ListElementType &newHistory)
     // precondition: list is in order
     Link addedNode(new Node);
     assert(addedNode);
+    // copy struct data
     addedNode->history = newHistory;
 
-    // Special case: if the existing list is empty, or if the new data
-    // is less than the smallest item in the list, the new node is added
-    // to the front of the list
-    if (head == 0 || newHistory.hits <= head->history.hits) { //최초로 넣는 경우, 내가 가장 작을 떄
-        addedNode->next = head;
-        head = addedNode;
-    }
-    else { //중간 적당한 위치에 넣는 경우
-        // find the pointer to the node that is the predecessor
-        // to the new node in the in-order list
-        Link pred(head);
-        // assertion: pred->elem <= addedNode->elem
-        while (pred->next != 0 && pred->next->history.hits <= addedNode->history.hits)
-            // loop invariant: pred->next != 0 && pred->next->elem <= elem
-            pred = pred->next;
-        // assertion 7-1: (pred->elem <= addedNode->elem) &&
-        //    (addedNode->elem <= pred->next->elem || pred->next == 0) //pred가 새로운것 보다 작은지, 끝까지 도달했는지
-        addedNode->next = pred->next;
-        pred->next = addedNode;
-        // assertion: pred->elem <= pred->next->elem &&
-        //    (pred->next->elem <= pred->next->next->elem || pred->next->next == 0)
-    }
+    // find the pointer to the node that is the predecessor
+    // to the new node in the in-order list
+    Link pred(head);
+    // assertion: pred->elem <= addedNode->elem
+    while (pred->next != 0 && pred->next->history.hits <= addedNode->history.hits)
+        // loop invariant: pred->next != 0 && pred->next->elem <= elem
+        pred = pred->next;
+    // assertion 7-1: (pred->elem <= addedNode->elem) &&
+    //    (addedNode->elem <= pred->next->elem || pred->next == 0) //pred가 새로운것 보다 작은지, 까지 도달했는지
+    addedNode->next = pred->next;
+    pred->next = addedNode;
+    // assertion: pred->elem <= pred->next->elem &&
+    //    (pred->next->elem <= pred->next->next->elem || pred->next->next == 0)
+    
     // postcondition: list is in order, with elem added in proper position
 }
 
 void Search::printAllHistory()
 {
+    assert(head); // if no head, something is very wrong!
     int index = 0;
-    current = head;
+    current = head->next;
     
-    if(head == 0){
+    if(head->next == 0){
         cout << "No saved history" << endl;
         return;
     }
     
     while(current != 0){
-        
         cout << "Index: " << index << endl;
         cout << "-Keyword: " << current->history.keyWord \
         << "\n-Final webpage: " << current->history.finalWebPage \
